b5ef6e7bb0d40ace5f6afef2372fb9bb5a3c7969
Gowtham-matheswaran <139969985+Gowtham-matheswaran@users.noreply.github.com>
2023-11-22 14:41:38 +0530

    Add Selenium project

---
+++ Sample.java
@@ -0,0 +1,19 @@
+package com.org.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:/Users/elcot/Browser/Chromedriver116/chromedriver-win32/Chromedriver.exe");
+		ChromeOptions co=new ChromeOptions();
+		co.addArguments("--remote-allow-origins=*");
+		co.addArguments("start-maximized");
+		WebDriver driver=new ChromeDriver(co);
+		driver.get("https://www.facebook.com/");
+		
+
+	}
+}
